a3cf9ede545712626b307e17ccaa5c4b444403b6
Sachin Tyagi <sachintyagi6322@gmail.com>
2026-07-14 22:57:23 +0530

    Add activity selection algorithm implementation
    Implemented the activity selection algorithm to maximize the number of non-overlapping activities based on their start and finish times.

--- ActivitySelection.java
+++ ActivitySelection.java
@@ -26,3 +26,83 @@ public class ActivitySelection {
   }
   
 }
+
+
+
+
+
+
+
+// import java.util.*;
+
+// public class ActivitySelection {
+
+//     // Activity class
+//     static class Activity {
+//         int start;
+//         int finish;
+
+//         Activity(int start, int finish) {
+//             this.start = start;
+//             this.finish = finish;
+//         }
+//     }
+
+//     public static void main(String[] args) {
+
+//         Scanner sc = new Scanner(System.in);
+
+//         // Number of activities
+//         System.out.print("Enter number of activities: ");
+//         int n = sc.nextInt();
+
+//         Activity[] activities = new Activity[n];
+
+//         // Input start and finish time
+//         System.out.println("Enter start time and finish time:");
+
+//         for (int i = 0; i < n; i++) {
+//             int start = sc.nextInt();
+//             int finish = sc.nextInt();
+
+//             activities[i] = new Activity(start, finish);
+//         }
+
+//         // Sort activities based on finish time
+//         Arrays.sort(activities, Comparator.comparingInt(a -> a.finish));
+
+//         // Select first activity
+//         int count = 1;
+//         int lastFinish = activities[0].finish;
+
+//         System.out.println("\nSelected Activities:");
+//         System.out.println("(" + activities[0].start + ", " + activities[0].finish + ")");
+
+//         // Traverse remaining activities
+//         for (int i = 1; i < n; i++) {
+
+//             // If current activity starts after last selected activity finishes
+//             if (activities[i].start > lastFinish) {
+
+//                 // Select current activity
+//                 count++;
+
+//                 System.out.println("(" + activities[i].start + ", " + activities[i].finish + ")");
+
+//                 // Update last finish time
+//                 lastFinish = activities[i].finish;
+//             }
+//         }
+
+//         System.out.println("\nMaximum Activities = " + count);
+
+//         sc.close();
+//     }
+// }
+
+
+
+// I first store each activity as a pair of start and finish times. Then I sort all activities by their finish time because choosing the activity
+// that finishes earliest leaves the maximum time for future activities. I always select the first activity, 
+// then iterate through the remaining activities. If the current activity starts after the last selected activity finishes, 
+// I select it and update the last finish time. This greedy strategy gives the maximum number of non-overlapping activities."
