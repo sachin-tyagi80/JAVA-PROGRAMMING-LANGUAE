182fa1f13b008ebed290417e87e8c19520edb24f
sachin-tyagi80 <sachintyagi6322@gmail.com>
2026-07-14 13:09:57 +0530

    Greedy Approach

---
+++ ActivitySelection.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+public class ActivitySelection {
+  public static void main(String[] args) {
+    int start[] = {1,3,0,5,8,5};
+    int end[] = {2,4,6,7,9,9};
+
+    //end time basis sorted
+    int maxAct = 0;
+    ArrayList<Integer> ans = new ArrayList<>();
+    
+    //1st activity
+    maxAct = 1;
+    ans.add(0);
+    int lastEnd = end[0];
+  
+    for(int i=1;i<end.length;i++){
+      if(start[i]>=lastEnd){
+        //activity selected
+        maxAct++;
+        ans.add(i);
+        lastEnd = end[i];
+      }  
+    }
+    System.out.println("Maximum activities selected: " + maxAct);
+    System.out.println("Selected activities (indices): " + ans);
+  }
+  
+}
