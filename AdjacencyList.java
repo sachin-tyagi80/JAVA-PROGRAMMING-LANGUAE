c685ad9478b598a6ed941e8c8d3c6914bd4f00e3
sachin-tyagi80 <sachintyagi6322@gmail.com>
2026-07-24 12:10:58 +0530

    Graph

--- AdjacencyList.java
+++ AdjacencyList.java
@@ -19,19 +19,29 @@ public class AdjacencyList {
     }
 
     // adding edges
+    // 0-vertex 
     graph[0].add(new Edge(0,1,5));
-    graph[0].add(new Edge(0,2,3));
-    graph[1].add(new Edge(1,3,8));
-    graph[2].add(new Edge(2,3,7));
-    graph[3].add(new Edge(3,4,2));
+    
+    // 1-vertex
+    graph[1].add(new Edge(1,0,5));
+    graph[1].add(new Edge(1,2,1));
+    graph[1].add(new Edge(1,3,3));
+    //2-vertex
+    graph[2].add(new Edge(2,1,1));
+    graph[2].add(new Edge(2,3,1));
+    graph[2].add(new Edge(2,4,4));
+
+    //3-vertex
+    graph[3].add(new Edge(3,1,3));
+    graph[3].add(new Edge(3,2,1));
+    //4-vertex
+    graph[4].add(new Edge(4,2,2));
 
     // printing the adjacency list
-    for(int i=0;i<n;i++){
-      System.out.print("Vertex " + i + " is connected to: ");
-      for(Edge e : graph[i]){
-        System.out.print("(" + e.dest + ", weight: " + e.weight + ") ");
-      }
-      System.out.println();
+    //2's neighbors
+    for(int i =0;i<graph[2].size();i++){
+     Edge e = graph[2].get(i);
+     System.out.println(e.dest );
     }
   }
   
